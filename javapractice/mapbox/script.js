52daf26785a32e756ca0061083a4dd66393f6fd1
rishi <rishireddy88291@gmail.com>
2024-06-02 13:42:39 +0530

    added the module and mapbox

---
+++ javapractice/mapbox/script.js
@@ -0,0 +1,32 @@
+const MAPBOX_ACCESS_TOKEN =
+'pk.eyJ1IjoibGF3aXgxMCIsImEiOiJjamJlOGE1bmcyZ2V5MzNtcmlyaWRzcDZlIn0.ZRQ73zzVxwcADIPvsqB6mg';
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+  enableHighAccuracy: true
+})
+
+function setupMap(centerPosition) {
+  const map = new mapboxgl.Map({
+    accessToken: MAPBOX_ACCESS_TOKEN,
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: centerPosition,
+    zoom: 15
+  })
+
+  const navigationControls = new mapboxgl.NavigationControl()
+  map.addControl(navigationControls)
+
+  const directionControls = new MapboxDirections({
+    accessToken: MAPBOX_ACCESS_TOKEN
+  })
+  map.addControl(directionControls, "top-left")
+}
+
+function successLocation(position) {
+  setupMap([position.coords.longitude, position.coords.latitude])
+}
+
+function errorLocation() {
+  setupMap([-2.24, 53.48])
+}
